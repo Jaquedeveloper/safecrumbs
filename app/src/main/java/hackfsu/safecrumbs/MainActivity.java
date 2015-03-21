17daa86a59ba8588f4fb2061fa07c8f517a4a1db
Jaqueline <jaqueline.pnascimento@gmail.com>
2015-03-21 13:22:48 -0400

    Choose contacts to send message

--- app/src/main/java/hackfsu/safecrumbs/MainActivity.java
+++ app/src/main/java/hackfsu/safecrumbs/MainActivity.java
@@ -9,10 +9,14 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.util.Log;
+import android.view.View.OnClickListener;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 
 public class MainActivity extends ActionBarActivity {
     final String preferenceName = "MyPreferenceFile";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,14 +49,33 @@ public class MainActivity extends ActionBarActivity {
 
     public void checkLogging(){
         SharedPreferences settings = getSharedPreferences(preferenceName, 0);
+
         boolean isFirstTime = settings.getBoolean("isFirstTime", true);
+        buildAlertDialog("Shared",Boolean.toString(isFirstTime));
         if (isFirstTime) {
             Intent intent = new Intent("com.Android.main_911");
-            Log.d("Comments", "First time");
-            settings.edit().putBoolean("isFirstTime", false).commit();
-
+            //startActivity(intent);
         }else{
-            Intent intent = new Intent("com.Android.main_911");
+            settings.edit().putBoolean("isFirstTime", false).commit();
+            final int request_code = 1010;
+            startActivityForResult(new Intent(MainActivity.this,ContactPicker.class),request_code);
         }
     }
+
+    public void buildAlertDialog(String title,String message){
+        // 1. Instantiate an AlertDialog.Builder with its constructor
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        // 2. Chain together various setter methods to set the dialog characteristics
+        builder.setMessage(message)
+                .setTitle(title)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+        // 3. Get the AlertDialog from create()
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 }
